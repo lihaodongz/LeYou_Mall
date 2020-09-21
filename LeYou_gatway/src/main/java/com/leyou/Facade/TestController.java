8840da21642b38dcc880f9e262a9555e1c434cbf
lihaodongz <799193211@qq.com>
2020-09-21 22:52:57 +0800

    goods 模块设置 feign 简单调用

---
+++ LeYou_gatway/src/main/java/com/leyou/Facade/TestController.java
@@ -0,0 +1,24 @@
+package com.leyou.Facade;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author 李浩东
+ * @description
+ * @date 2020/9/21 22:30
+ **/
+@RestController
+public class TestController {
+
+    @Resource
+    private FeiginClientTest FeiginClientTest;
+
+    @RequestMapping("/index")
+    public String get(){
+        return FeiginClientTest.hello();
+    }
+}
